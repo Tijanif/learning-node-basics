46c0a3cf41499716fbf9deb838a453f91a6e2b60
Tijani Fulani <Fulani07@gmail.com>
2021-07-20 16:25:03 +0100

    server

--- Clients and Servers/server.js
+++ Clients and Servers/server.js
@@ -10,3 +10,7 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
   console.log('Listening for requests om port 3000');
 });
+
+// loop back ip address 127.0.01 localhost using our own computer.
+
+// Port numbers represents a channel, gateway or port on ower computer - letting a certain bit of program to communicate with. Port numbers are like 'doors'
