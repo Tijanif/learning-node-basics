25c4063c0ea3b1707fbbcbd548b61c727ce59c00
Tijani Fulani <Fulani07@gmail.com>
2021-07-20 16:43:38 +0100

    Sending html pages from the server

--- Clients and Servers/server.js
+++ Clients and Servers/server.js
@@ -1,9 +1,29 @@
 // Node module
 const http = require('http');
+const fs = require('fs');
 
 // Creating the server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-type', 'text/html');
+
+  // Setting the content
+  // res.write('<p>Hello, Diana</p>');
+  // res.write('<p>Hello, Tijani</p>');
+
+  // Sending HTML files
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      // Ending the respons
+      res.end();
+    }
+  });
 });
 
 // Listening for request
