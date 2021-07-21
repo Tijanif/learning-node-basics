660bc712b40e55cc8e20de2c37b45136c7a04fa2
Tijani Fulani <Fulani07@gmail.com>
2021-07-21 09:31:26 +0100

    .

--- Basics/files.js
+++ Basics/files.js
@@ -16,23 +16,23 @@ fs.readFile('./docs/blog1.txt')
 
 // Writing Files
 
-fs.writeFile('./docs/blog1.txt', 'Hello, World!').then(() => {
-  console.log('file was written');
-});
+// fs.writeFile('./docs/blog1.txt', 'Hello, World!').then(() => {
+//   console.log('file was written');
+// });
 
-// Writing to files that are not there - node create the file for you
-fs.writeFile('./docs/blog2.txt', 'Hello, I am in blog two!').then(() => {
-  console.log('file was written');
-});
+// // Writing to files that are not there - node create the file for you
+// fs.writeFile('./docs/blog2.txt', 'Hello, I am in blog two!').then(() => {
+//   console.log('file was written');
+// });
 
 // Directories creating a new folder
-fs.mkdir('./assets')
-  .then(() => {
-    console.log('folder created');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fs.mkdir('./assets')
+//   .then(() => {
+//     console.log('folder created');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // Cheaking of file exist
 // if (!fs.exists('./assets')) {
@@ -46,11 +46,11 @@ fs.mkdir('./assets')
 // }
 
 // Removing directory
-fs.rmdir('./assets').then(() => {
-  console.log('folder removed');
-});
+// fs.rmdir('./assets').then(() => {
+//   console.log('folder removed');
+// });
 
-// Deleting Files
-fs.unlink('./docs/deletme.txt').then(() => {
-  console.log('I was deleted');
-});
+// // Deleting Files
+// fs.unlink('./docs/deletme.txt').then(() => {
+//   console.log('I was deleted');
+// });
